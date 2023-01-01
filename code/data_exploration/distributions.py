c7af09802d3bff89e3ee1153fa36332f57dc4a01
Maximilian Schattauer <max@schattauer.info>
2023-01-01 20:27:54 +0100

    Added data balancing

--- code/data_exploration/distributions.py
+++ code/data_exploration/distributions.py
@@ -32,13 +32,32 @@ def balance_role_models(data, n_target = 50, downsample=True, upsample=True):
         if upsample and len(role_model_data) < n_target:
             full_repetitions = n_target // len(role_model_data)
             additional = role_model_data.loc[[*role_model_data.index]*full_repetitions].reset_index()
-            additional = additional.iloc[0:n_target]
             additional.index = 1000000000 + role_model_data.index.min() + additional.index
-            role_model_data = pd.concat([role_model_data, additional])
+            role_model_data = pd.concat([role_model_data, additional]).iloc[0:n_target]
 
         new_data = pd.concat([new_data, role_model_data])
     return new_data
 balanced = balance_role_models(articles_en)
+
+
+# %%
+# Article per role model distribution
+role_models, counts = np.unique(articles['role_model'], return_counts=True)
+role_models_balanced, counts_balanced = np.unique(balanced['role_model'], return_counts=True)
+sorted_counts, sorted_counts_balanced = np.sort(counts), np.sort(counts_balanced)
+counts_cumulative, counts_cumulative_balanced = sorted_counts.cumsum(), sorted_counts_balanced.cumsum()
+plt.title('Articles per role model')
+plt.xlabel('percentile')
+plt.ylabel('Cumulative relative amount of articles')
+plt.plot(np.arange(counts_cumulative.size)/(counts_cumulative.size)*100, counts_cumulative/np.max(counts_cumulative)*100, label='data')
+plt.plot(np.arange(counts_cumulative_balanced.size)/(counts_cumulative_balanced.size)*100, counts_cumulative_balanced/np.max(counts_cumulative_balanced)*100, label='balanced data')
+plt.gca().xaxis.set_major_formatter(PercentFormatter())
+plt.gca().yaxis.set_major_formatter(PercentFormatter())
+plt.margins(x=0, y=0)
+plt.grid()
+plt.legend()
+plt.show()
+
 
 # %%
 # Language distribution
@@ -46,22 +65,6 @@ role_models, counts = np.unique(articles['language_ml'], return_counts=True)
 plt.title('Language distribution')
 plt.bar(role_models, counts/np.sum(counts)*100, align='center')
 plt.gca().yaxis.set_major_formatter(PercentFormatter())
-plt.show()
-
-
-# %%
-# Article per role model distribution
-role_models, counts = np.unique(articles['role_model'], return_counts=True)
-sorted_counts = np.sort(counts)
-count_cumulative = sorted_counts.cumsum()
-plt.title('Articles per role model')
-plt.xlabel('percentile')
-plt.ylabel('Cumulative relative amount of articles')
-plt.plot(np.arange(count_cumulative.size)/(count_cumulative.size)*100, count_cumulative/np.max(count_cumulative)*100)
-plt.gca().xaxis.set_major_formatter(PercentFormatter())
-plt.gca().yaxis.set_major_formatter(PercentFormatter())
-plt.margins(x=0, y=0)
-plt.grid()
 plt.show()
 
 
