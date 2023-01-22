2332a3dcfdc0c00195692fc3d6d95d746fc3cf21
Maximilian Schattauer <max@schattauer.info>
2023-01-22 13:48:29 +0100

    Added AWS sync

--- aws_sync.sh
+++ aws_sync.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 aws s3 sync "/home/maxi/Programmieren/vwl/econ_bachelors_thesis/" "s3://econ-bachelors-thesis/" --exclude '*' \
+    --include "build/articles/articles.pkl" \
     --include "build/articles/articles_balanced_50.pkl" \
-    --include "build/zero_shot_classification/zero_shot_classification.pkl" \
-    --include "code/tasks/task_zero_shot.py"
+    --include "build/articles/articles_human_annotated.pkl" \
+    --include "build/zero_shot_classification/*" \
+    --include "build/topic_modelling/*"
