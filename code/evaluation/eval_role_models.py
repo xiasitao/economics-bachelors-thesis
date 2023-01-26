7c63c97e72e2299b7ac9de98d4e01ff8bd09cbb0
Maximilian Schattauer <max@schattauer.info>
2023-01-26 18:04:51 +0100

    First part of data ch/ses

---
+++ code/evaluation/eval_role_models.py
@@ -0,0 +1,25 @@
+# %%
+import pandas as pd
+import numpy as np
+
+from pathlib import Path
+SOURCE_PATH = Path(__file__).parent.resolve()
+ASSET_PATH = SOURCE_PATH.joinpath('..', '..', 'assets').resolve()
+BUILD_PATH = SOURCE_PATH.joinpath("..", "..", "build").resolve()
+
+
+# %%
+ses = pd.read_pickle(BUILD_PATH / 'role_models/ses.pkl')
+print('Number of children with sensible role models:', len(ses))
+mentions = pd.read_pickle(BUILD_PATH / 'role_models/ses_mentions.pkl')
+print('Number of mentions: ', len(mentions))
+print('Mentions by rank:', mentions.groupby('rank').count()['id'])
+
+
+# %%
+role_model_data = pd.read_pickle(BUILD_PATH / 'role_models/role_model_data.pkl')
+print('Number of role models:', len(role_model_data))
+print('Role models by gender:', [f'{gender}: {number}' for (gender, number) in zip(*np.unique(role_model_data['sex'].fillna(2), return_counts=True))])
+
+
+# %%
