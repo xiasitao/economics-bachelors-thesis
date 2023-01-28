671a49dc8604c52590888965e62b60fa07de4f1d
Maximilian Schattauer <max@schattauer.info>
2023-01-28 21:35:47 +0100

    Resturctured repo a bit

--- ec2_user_data.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-cd /home/ec2-user
-echo "aws s3 sync s3://econ-bachelors-thesis/ econ_bachelors_thesis" > s3_sync.sh
-chmod +x s3_sync.sh
-sh s3_sync.sh
-chown -R ec2-user econ_bachelors_thesis
-
-amazon-linux-extras install python3.8
-python3.8 -m pip install pytask pandas==1.4.3 numpy transformers torch
