81af8ca6b0a4c5d61c29b321437a841bdcfb3963
Maximilian Schattauer <max@schattauer.info>
2023-02-14 00:10:47 +0100

    First run of proofreading

--- code/evaluation/thesis_content_data.py
+++ code/evaluation/thesis_content_data.py
@@ -40,13 +40,13 @@ def produce_role_model_list(produces: Path):
     role_model_data = role_model_data.join(scores, how='inner')
     role_model_data = role_model_data.sort_index(key=lambda name: name.str.lower())
 
-    latex = r"\begin{longtable}{lccccccc}\toprule role model & sex & main prof. & y.o.b. & \#low & \#high & $\in \mathcal{R}_\text{distinct}$ \\\toprule"
+    latex = r"\begin{longtable}{lccccccc}\toprule role model & sex & main prof. & y.o.b. & \#low & \#high & $\textit{distinct}$ \\\toprule"
     for i in range(len(role_model_data)):
         role_model = role_model_data.iloc[i].fillna("")
-        line = f'{role_model.name} & {"m" if role_model["sex"]==0.0 else "f"} & {role_model["profession"]} & {role_model["birth_year"]} & {role_model["low_ses_count"]} & {role_model["high_ses_count"]} & {"$XYZXYZnotin$" if role_model["low_ses"] and role_model["high_ses"] else "$XYZXYZin$"}'
+        line = f'{role_model.name} & {"m" if role_model["sex"]==0.0 else "f"} & {role_model["profession"]} & {role_model["birth_year"]} & {role_model["low_ses_count"]} & {role_model["high_ses_count"]} & {"XYZXYZxmark" if role_model["low_ses"] and role_model["high_ses"] else r"XYZXYZcmark"}'
         line = line.replace('XYZXYZ', '\\')
         latex += f'{line} \\\\\n'
-    latex += r"\bottomrule\caption{Overview of role models. Abbreviations: \textit{nat.}: nationality, \textit{main prof.}: main profession, \textit{y.o.b.}: year of birth, \textit{\#low/\#high}: number of mentions by low-SES/high-SES study participants, $\in \mathcal{R}_\text{distinct}$: whether they are in the set of role models with distinct SES association.}\label{tab:role_model_overview}\end{longtable}"
+    latex += r"\bottomrule\caption{Overview of role models. Abbreviations: \textit{nat.}: nationality, \textit{main prof.}: main profession, \textit{y.o.b.}: year of birth, \textit{\#low/\#high}: number of mentions by low-SES/high-SES study participants, \textit{distinct}: whether they are in the set of role models with distinct SES association.}\label{tab:role_model_overview}\end{longtable}"
     produces.write_text(latex)
 
 
